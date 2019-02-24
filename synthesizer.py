92407fa4c0a30a8230285429aef29e09340deddf
Runpeng Liu <rliu42@mit.edu>
2019-02-24 09:35:55 -0500

    Attenuate pure tones.

--- synthesizer.py
+++ synthesizer.py
@@ -1,7 +1,8 @@
-import pysynth_b as ps_piano
 import pysynth as ps
 from piano import *
+import os
+
+os.system("rm pure_tones/*.wav")
 
 for (i, k) in enumerate(KEYS):
-	ps.make_wav([(k.lower(), 4)], fn="pure_tones/{}.wav".format(i+1))
-	ps_piano.make_wav([(k.lower(), 4)], fn="piano_tones/{}.wav".format(i+1))
+	ps.make_wav([(k.lower() + "*", 4)], fn="pure_tones/{}.wav".format(i+1), boost=0.001)
